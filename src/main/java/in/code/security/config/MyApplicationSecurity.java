489800438d669076acaccbfe3de3a35fac6d4fea
Faisal Khan <faisal01khan2003@gmail.com>
2025-11-29 12:52:23 +0530

    basic http security

--- src/main/java/in/code/security/config/MyApplicationSecurity.java
+++ src/main/java/in/code/security/config/MyApplicationSecurity.java
@@ -6,6 +6,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,8 +24,28 @@ public class MyApplicationSecurity {
                 authorize -> authorize.requestMatchers(HttpMethod.DELETE, "/employee/delete/{id}")
                         .hasAnyRole("ADMIN")
                         .requestMatchers("/**").permitAll())
-                .formLogin(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                .username("faisal khan")
+                .password(passwordEncoder().encode("Faisal Khan"))
+                .roles("ADMIN")
+                .build();
+        UserDetails userDetails2 = User.builder()
+                .username("Guest")
+                .password(passwordEncoder().encode("guest"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails, userDetails2);
     }
 }
